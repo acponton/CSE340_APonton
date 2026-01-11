dabb331816b712cca46d03b08f04a40e7f24c064
acponton <pon22008@byui.edu>
2026-01-10 23:46:43 -0600

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(static)
 
 //Index Route
 app.get("/", function(req, res) {
-  res.render("index", {title: "Andrea"})
+  res.render("index", {title: "CSE Motors"})
 })
 
 /* ***********************
