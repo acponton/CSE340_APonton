3ac030853928d230043ece3bdb6d0674468139ae
acponton <pon22008@byui.edu>
2026-02-05 23:08:24 -0600

    Week4_TeamActivity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -11,6 +11,8 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
+// router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
 // Process the registration data
 router.post("/register", regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
